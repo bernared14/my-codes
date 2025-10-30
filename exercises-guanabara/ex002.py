7d6fe4310f5ee2f6ddd95825e3105cb20bdc069a
bernared14 <81765951+bernared14@users.noreply.github.com>
2025-10-30 16:15:16 -0400

    Add welcome message program in multiple languages

---
+++ exercises-guanabara/ex002.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas. 🇧🇷
+# Write a program that reads a person's name and displays a welcome message. 🇺🇸
+# Escribe un programa que lea el nombre de una persona y muestre un mensaje de bienvenida. 🇪🇸
+# Écrivez un programme qui lit le nom d'une personne et affiche un message de bienvenue. 🇫🇷
+
+nome = str(input("Enter your name: "))
+print(f"Hello, {nome}!")
