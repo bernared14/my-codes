8d2ad71174007887c87bcfd97ef97036b8937dae
bernared14 <81765951+bernared14@users.noreply.github.com>
2025-10-30 16:14:32 -0400

    Create Hello World program in ex001.py
    Add a program to display 'Hello, World!' in multiple languages.

---
+++ exercises-guanabara/ex001.py
@@ -0,0 +1,6 @@
+# Crie um programa que mostre o "Hello, World!" na tela. 🇧🇷
+# Create a program that displays "Hello, World!" on the screen. 🇺🇸
+# Crea un programa que muestre "Hello, World!" en la pantalla. 🇪🇸
+# Créez un programme qui affiche « Hello, World! » à l'écran. 🇫🇷
+
+print("Hello, World!")
