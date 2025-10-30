4c2df0937417a06c7b9f41a2cc00ee8d20dad1e5
bernared14 <81765951+bernared14@users.noreply.github.com>
2025-10-30 16:16:24 -0400

    Delete exercises directory

--- exercises/ex003.py
+++
@@ -1,9 +0,0 @@
-# Crie um programa que leia dois números e mostre a soma entre eles. 🇧🇷
-# Write a program that reads two numbers and displays their sum. 🇺🇸
-# Escribe un programa que lea dos números y muestre su suma. 🇪🇸
-# Écrivez un programme qui lit deux nombres et affiche leur somme. 🇫🇷
-
-n1 = int(input("Enter a first number:: "))
-n2 = int(input("Enter a second number: "))
-soma = n1 + n2
-print(f"The sum of the numbers {n1} and {n2} is equal to {soma}.")
